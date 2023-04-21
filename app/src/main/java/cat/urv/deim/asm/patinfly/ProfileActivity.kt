d89366bc48785c205a6764c5c8800ca1de1f647c
AlejLinXu <zhenghaoalejandro.lin@estudiants.urv.cat>
2023-04-21 23:25:49 +0200

    Commit

--- app/src/main/java/cat/urv/deim/asm/patinfly/ProfileActivity.kt
+++ app/src/main/java/cat/urv/deim/asm/patinfly/ProfileActivity.kt
@@ -2,19 +2,45 @@ package cat.urv.deim.asm.patinfly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import cat.urv.deim.asm.patinfly.databinding.ActivityProfileBinding
 
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity(), ProfileView {
 
     private lateinit var binding: ActivityProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        hideProgress()
         loadSetData()
 
+        binding.btnEditProfile.setOnClickListener(){
+            postDelayed(500){
+                editable()
+            }
+
+        }
+
+    }
+
+    private fun editable(){
+        val name = binding.etFirstname
+        val last = binding.etLastname
+        val iD = binding.etID
+        val phoneNum = binding.etPhoneNum
+        val kil = binding.etKm
+        val mail = binding.etEmail
+        val nation = binding.etNationality
+
+        name.isEnabled = true
+        last.isEnabled = true
+        iD.isEnabled = true
+        phoneNum.isEnabled = true
+        kil.isEnabled = true
+        mail.isEnabled = true
+        nation.isEnabled = true
 
     }
     private fun loadSetData(){
@@ -29,7 +55,6 @@ class ProfileActivity : AppCompatActivity() {
 
         val user = intent.getSerializableExtra("userList") as? User
         val firstName = user?.firstName
-        Log.d("name", firstName.toString())
         val lastName = user?.lastName
         val email = user?.email
         val phone = user?.phone
@@ -53,4 +78,17 @@ class ProfileActivity : AppCompatActivity() {
         mail.isEnabled = false
         nation.isEnabled = false
     }
+
+    override fun showProgress() {
+        binding.progressBar.visibility = View.VISIBLE
+    }
+
+    override fun hideProgress() {
+        binding.progressBar.visibility = View.INVISIBLE
+    }
+
+    override fun navigateToProfile() {
+        TODO("Not yet implemented")
+    }
+
 }
--- app/src/main/java/cat/urv/deim/asm/patinfly/ProfileView.kt
+++ app/src/main/java/cat/urv/deim/asm/patinfly/ProfileView.kt
@@ -3,12 +3,5 @@ package cat.urv.deim.asm.patinfly
 interface ProfileView {
     fun showProgress()
     fun hideProgress()
-    fun setFirstNameError()
-    fun setLastNameError()
-    fun setEmailError()
-    fun setPhoneError()
-    fun setIDError()
-    fun setNationalityError()
-    fun setKmError()
     fun navigateToProfile()
 }
