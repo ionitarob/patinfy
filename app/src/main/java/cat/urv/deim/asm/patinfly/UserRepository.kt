17b6adbe2a9ed12618eec8cdab715169b0ca2b2f
AlejLinXu <zhenghaoalejandro.lin@estudiants.urv.cat>
2023-06-17 17:43:51 +0200

    commit

--- app/src/main/java/cat/urv/deim/asm/patinfly/UserRepository.kt
+++ app/src/main/java/cat/urv/deim/asm/patinfly/UserRepository.kt
@@ -1,15 +1,20 @@
 package cat.urv.deim.asm.patinfly
 
-class UserRepository{
-    private val userList = mutableListOf<User>()
+import androidx.room.Query
 
+class UserRepository{
     fun addUser(userDao: UserDao, user: User){
         return userDao.insert(user)
     }
-    fun getLastUser(): User {
-        return userList.last()
-    }
     fun getUserByID (userDao: UserDao, id: String): User {
         return userDao.getUserByID(id)
+    }
+
+    fun getUserByEmail(userDao: UserDao, userEmail: String): Boolean{
+        return userDao.getUserByEmail(userEmail)
+    }
+
+    fun getUserByPw(userDao: UserDao, userPw: String): Boolean{
+        return userDao.getUserByPw(userPw)
     }
 }
